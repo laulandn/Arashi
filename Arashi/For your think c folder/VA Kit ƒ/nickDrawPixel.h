e2a6e33e0d871a280cf7d3ed51e9a53f172d56d8
Nick Lauland <laulandn@gmail.com>
2023-01-25 10:48:39 -0600

    Non-solid lines, ORIG_ARASHI code back, etc

---
+++ Arashi/For your think c folder/VA Kit ƒ/nickDrawPixel.h
@@ -0,0 +1,1 @@
+#ifdef NICK_256_NOT_1  addr=VA.quickrow[y]+x;  *addr=1;#else  addr=VA.quickrow[y];  /*if(x<0) return;  if(y<0) return;*/  addr+=(x/8);  draw=0x80>>(x%8);  *addr=(*addr)&(~draw);#endif
